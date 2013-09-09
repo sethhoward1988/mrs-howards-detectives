58b6e073141cd1f61868db071a99f919d982cead
Seth Howard <smiles.seth@gmail.com>
2013-09-09 08:46:26 -0600

    added definitions to the words

--- spelling-quiz/js/data.js
+++ spelling-quiz/js/data.js
@@ -1,3 +1,86 @@
+currentWeek = [
+    {
+        word: 'sign',
+        definition: ' an object, quality, or event whose presence or occurrence indicates the probable presence or occurrence of something else'
+    },
+    {
+        word:'groan',
+        definition:'make a deep inarticulate sound in response to pain or despair'
+    },
+    {
+        word:'reply',
+        definition:'say something in response to something someone has said'
+    },
+    {
+        word:'thrown',
+        definition:'propel (something) with force through the air by a movement of the arm and hand'
+    },
+    {
+        word: 'strike',
+        definition:'hit forcibly and deliberately with one\'s hand or a weapon or other implement'
+    },
+    {
+        word:'mighty',
+        definition:'possessing great and impressive power or strength, esp on account of size'
+    },
+    {
+        word:'stroll',
+        definition:'walk in a leisurely way'
+    },
+    {
+        word:'compose',
+        definition:'write or create (a work of art, esp. music or poetry)'
+    },
+    {
+        word:'dough',
+        definition: 'a thick, malleable mixture of flour and liquid, used for baking into bread or pastry'
+    },
+    {
+        word:'height',
+        definition:'the measurement from base to top or (of a standing person) from head to foot'
+    },
+    {
+        word:'excite',
+        definition:'cause strong feelings of enthusiasm and eagerness in (someone)'
+    },
+    {
+        word:'apply',
+        definition:'make a formal application or request'
+    },
+    {
+        word:'slight',
+        definition:'small in degree; inconsiderable'
+    },
+    {
+        word:'define',
+        definition:'state or describe exactly the nature, scope, or meaning of'
+    },
+    {
+        word:'odor',
+        definition:'a distinctive smell, esp. an unpleasant one'
+    },
+    {
+        word:'spider',
+        definition:'an eight-legged predatory arachnid'
+    },
+    {
+        word:'control',
+        definition:'the power to influence or direct people\'s behavior or the course of events'
+    },
+    {
+        word:'silent',
+        definition:'not making or accompanied by any sound'
+    },
+    {
+        word:'brighten',
+        definition:'make or become more light'
+    },
+    {
+        word:'approach',
+        definition:'come near or nearer to (someone or something) in distance'
+    }
+]
+
 group1 = ['sign', 'groan', 'reply', 'thrown', 'strike']
 group2 = ['mighty', 'stroll', 'compose', 'dough', 'height']
 group3 = ['excite', 'apply', 'slight', 'define', 'odor']
